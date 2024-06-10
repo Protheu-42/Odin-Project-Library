c6320d0dbda613b6b79c266f5ecf2e0a0e307490
Protheu <everaldo.sobrinho42@gmail.com>
2024-06-10 12:49:32 -0300

    Code refacored to work as Class

--- script.js
+++ script.js
@@ -11,15 +11,44 @@ closeDialogBtn.addEventListener("click", () => {
     dialog.close();
 })
 
-// DATA CAPTURING AND DISPLAY//
-function Book(title, author, pages, reading) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.reading = reading;
+
+class Book {
+    constructor(title, author, pages, reading) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.reading = reading;
+    }
 }
 
-let library = [];
+class Library {
+    constructor() {
+        this.library = [];
+    }
+
+    length() { 
+        return this.library.length;
+    }
+
+    addToLibrary(addThisBookToLibrary) {
+        this.library.push(addThisBookToLibrary);
+    }
+
+    getReading(index) {
+        return this.library[index].reading; 
+    }
+
+    setReading(index, status) {
+        this.library[index].reading = status;
+    }
+
+    deleteBook(index) {
+        this.library.splice(index, 1);
+    }
+
+}
+
+let userLibrary = new Library();
 
 const addBookBtn = document.querySelector("dialog footer button");
 
@@ -31,30 +60,23 @@ addBookBtn.addEventListener("click", function (event) {
     const reading = document.querySelector("#reading");
 
     addBook(title.value, author.value, pages.value, reading.checked ? true : false);
+    dialog.close();
+    clearValues(title, author, pages, reading);
 });
 
 addBook("The Hobbit", "J.R.R Tolkien", 297, true);
 
 
-// CHANGE AND REMOVE DATA INTERACTION
-const toggleReadingBtn = document.querySelectorAll(".book-card-content .toggle-btn");
-toggleReadingBtn.forEach(btn => {
-    btn.addEventListener("click", (event) => {
-        event.preventDefault();
-        const index = btn.parentNode.firstChild.lastChild.textContent;
-        const readingDiv = btn.previousSibling;
-        if (!library[index].reading) {
-            library[index].reading = true;
-            readingDiv = "Not Reading"
-        } else {
-            library[index].reading = false;
-        }
-        
-    })    
-});
-
-
 // GENERAL FUNCTIONS
+
+function clearValues(...args) {
+    args.forEach(element => {
+        element.value = "";
+        if (element.checked) {
+            element.checked = false;
+        }
+    });
+}
 
 function addBook(title, author, pages, reading) {
     const bookList = document.querySelector(".book-list")
@@ -65,20 +87,49 @@ function addBook(title, author, pages, reading) {
     const header = document.createElement("header");
     header.textContent = title;
     const index = document.createElement("span");
-    index.textContent = library.length;
+    index.textContent = userLibrary.length();
     const authorDiv = document.createElement("div");
     authorDiv.textContent = author;
     const pagesDiv = document.createElement("div")
     pagesDiv.textContent = pages;
+   
     const readingStatus = document.createElement("div")
     if (reading === true) {
         readingStatus.textContent = "Reading";
     } else {
         readingStatus.textContent = "Not Reading";
     }
+    
     const button = document.createElement("button");
     button.textContent = "Toggle Reading";
-    button.classList.add("toggle-btn")
+    button.classList.add("toggle-btn");
+    button.addEventListener("click", (event) => {
+        event.preventDefault();
+        const btn = event.target
+        const index = btn.parentNode.firstChild.lastChild.textContent;
+        const readingDiv = btn.previousSibling;
+        if (userLibrary.getReading(index) === false) {
+            userLibrary.setReading(index, true);
+            readingDiv.textContent = "Reading";
+        } else {
+            userLibrary.setReading(index, false);
+            readingDiv.textContent = "Not Reading";
+        }
+    })
+
+
+    const deleteBtn = document.createElement("button");
+    deleteBtn.textContent = "Delete";
+    deleteBtn.classList.add("delete");
+    deleteBtn.addEventListener("click", (event) => {
+        event.preventDefault();
+        const btn = event.target
+        const card = btn.parentNode.parentNode;
+        const index = btn.parentNode.firstChild.lastChild.textContent;
+        userLibrary.deleteBook(index);
+        card.remove()
+    })
+
 
     card.appendChild(bookCardContent);
     header.appendChild(index);
@@ -87,41 +138,8 @@ function addBook(title, author, pages, reading) {
     bookCardContent.appendChild(pagesDiv);
     bookCardContent.appendChild(readingStatus);
     bookCardContent.appendChild(button);
+    bookCardContent.appendChild(deleteBtn);
     bookList.appendChild(card);
     
-    library.push(new Book(title, author, pages, reading ? true : false));
+    userLibrary.addToLibrary(new Book(title, author, pages, reading ? true : false));
 }
-
-
-// function addBook(book) {
-//     const bookList = document.querySelector(".book-list")
-//     const card = document.createElement("div");
-//         card.classList.add("card");
-//         const bookCardContent = document.createElement("div");
-//         bookCardContent.classList.add("book-card-content");
-//         const header = document.createElement("header");
-//         header.textContent = book.title;
-//         const index = document.createElement("span");
-//         index.textContent = library.length - 1;
-//         const author = document.createElement("div");
-//         author.textContent = book.author;
-//         const pages = document.createElement("div")
-//         pages.textContent = book.pages;
-//         const readingStatus = document.createElement("div")
-//         if (book.reading === true) {
-//             readingStatus.textContent = "Reading";
-//         } else {
-//             readingStatus.textContent = "Not Reading";
-//         }
-//         const button = document.createElement("button");
-//         button.textContent = "Toggle Reading";
-
-//         card.appendChild(bookCardContent);
-//         header.appendChild(index);
-//         bookCardContent.appendChild(header);
-//         bookCardContent.appendChild(author);
-//         bookCardContent.appendChild(pages);
-//         bookCardContent.appendChild(readingStatus);
-//         bookCardContent.appendChild(button);
-//         bookList.appendChild(card);
-// }
